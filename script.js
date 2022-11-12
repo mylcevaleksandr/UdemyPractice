cb7696db3ea9ea76ba03a9e00ce7a3d17c1b61a0
mylcevaleksandr <myltsevaleksandr@gmail.com>
2022-11-13 00:38:19 +0300

    game

---
+++ script.js
@@ -0,0 +1,27 @@
+let Beer = 100;
+let ninetyNine = 99;
+function BeerBottles () {
+while (Beer >=0) {
+    if (Beer === 1){
+       console.log (" " + Beer + " bottle of beer on the wall , " + Beer + " bottle of beer.  Take one down and pass it around.");
+    } 
+    else{
+    console.log( " " + Beer + " bottles of beer on the wall , " + Beer + " bottles of beer.  Take one down and pass it around.");
+}
+    Beer--;
+    
+  if (Beer > 1 ){
+  console.log(Beer+ " bottles of beer on the wall.");
+}
+ if (Beer === 1) {
+        console.log(Beer + " bottle of beer on the wall.");
+    }
+    if (Beer === 0) {
+    console.log(Beer + " bottles of beer on the wall.");
+}
+ if (Beer === -1 ){
+    console.log(ninetyNine + " bottles of beer on the wall.");
+ }
+}
+}
+
